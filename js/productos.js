f49c268f2b77e0dc7174fa3cdaa7b27173e9070d
Jonathan Bedoya <jb_193@hotmail.com>
2023-07-04 21:29:00 -0500

    URL BD changed

--- js/productos.js
+++ js/productos.js
@@ -4,7 +4,7 @@ createApp({
     data(){
         return{
             productos: [],
-            url: 'http://127.0.0.1:5000/productos',
+            url: 'https://jonathanbedoya.pythonanywhere.com/productos',
             cargando: true,
             error: false 
         }
